d8191be567c8c7537f7af426682c5f928a5a7b8a
Fabio-Ramirez <fabioadiranr2@gmail.com>
2023-09-21 00:30:32 -0300

    first commit

---
+++ src/viewData.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const Lista = () => {
+    const [usuarios, setLista] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/comercio/users')
+            .then((resp) => {
+                const datos = resp.data;
+                setLista(datos);
+                console.log(resp.data);
+            })
+            .catch((error) => {
+                alert('Error obtendiendo los datos! Anda paya Bobo!!');
+                console.log(error);
+            })
+    }, []);
+
+    return (
+        <>
+            <h1>Lista de usuarios</h1>
+            <ul>
+                {usuarios.map((usuario) => (
+                    <li key={usuario.id}>
+                        {usuario.username} - {usuario.email}
+                    </li>
+                ))}
+            </ul>
+        </>
+
+    )
+}
